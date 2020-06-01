10a64b5da2d7ab73f4bac68be36bb58d71d00410
jk <kjkjk1178@gmail.com>
2020-06-01 22:32:02 +0900

    6.10.2장

--- board-example-frontend/src/router/index.js
+++ board-example-frontend/src/router/index.js
@@ -51,6 +51,30 @@ export default new Router({
       },
       props: {
         default: true
+      },
+      beforeEnter(to, from, next){
+        const {isAuthorized} = store.getters
+        if(!isAuthorized){
+          alert('로그인이 필요합니다!')
+          next({name: 'Signin'})
+          return false
+        }
+        store.dispatch('fetchPost', to.params.postId)
+          .then(() => {
+            const post = store.state.post
+            const isAuthor = post.user.id === store.state.me.id
+            if(isAuthor){
+              next()
+            }
+            else{
+              alert('게시물 작성자만 게시물을 수정할 수 있습니다.')
+              next(from)
+            }
+          })
+          .catch(err => {
+            alert(err.response.data.msg)
+            next(from)
+          })
       }
     },
     {
